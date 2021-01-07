2da3086b1eb1956ec19bb5ebafbdd950488ee2f8
janhamza <76878335+janhamza@users.noreply.github.com>
2021-01-07 14:35:56 +0100

    Add files via upload

--- tpchablais/index.js
+++ tpchablais/index.js
@@ -1,7 +1,10 @@
 var mymap = L.map('map').setView([46.27015750337884, 6.976565481307841], 11);
 
 // Limiter la carte au Chablais
-mymap.setMaxBounds([[46.69006771229332, 7.667337337893923], [45.89003415151929, 6.388643918529753]]);
+mymap.setMaxBounds([
+  [46.69006771229332, 7.667337337893923],
+  [45.89003415151929, 6.388643918529753]
+]);
 mymap.setMinZoom(9);
 mymap.setMaxZoom(15);
 
@@ -38,27 +41,27 @@ var icones = {};
 icones['terminus'] = L.icon({
   iconUrl: 'icones/terminus.png',
   zIndexOffset: 1,
-  iconSize:     [15, 16], // size of the icon
-  });
+  iconSize: [15, 16], // size of the icon
+});
 
 // Icone pour les arrêts-sur-demande
 icones['arret'] = L.icon({
   iconUrl: 'icones/arret.png',
-  iconSize:     [8, 8], // size of the icon
-  });
+  iconSize: [8, 8], // size of the icon
+});
 
 // Icone pour les vrais arrets
 icones['Varret'] = L.icon({
   iconUrl: 'icones/arret.png',
-  iconSize:     [12, 12], // size of the icon
+  iconSize: [12, 12], // size of the icon
 });
 
 // Icones pour le ski
 icones['ski'] = L.icon({
   iconUrl: 'icones/ski.png',
   iconSize: [28, 41],
-  iconAnchor:   [14, 40],
-  popupAnchor:  [0, -40]
+  iconAnchor: [14, 40],
+  popupAnchor: [0, -40]
 });
 
 // Icones selectionnées
@@ -68,100 +71,106 @@ var icones_selec = {};
 icones_selec['terminus'] = L.icon({
   iconUrl: 'icones/terminus_selec.png',
   zIndexOffset: 1,
-  iconSize:     [40, 40], // size of the icon
-  });
+  iconSize: [40, 40], // size of the icon
+});
 
 // Icone pour les arrêts-sur-demande
 icones_selec['arret'] = L.icon({
   iconUrl: 'icones/arret_selec.png',
-  iconSize:     [25, 25], // size of the icon
-  });
+  iconSize: [25, 25], // size of the icon
+});
 
 // Icone pour les vrais arrets
 icones_selec['Varret'] = L.icon({
   iconUrl: 'icones/arret_selec.png',
-  iconSize:     [32, 32], // size of the icon
+  iconSize: [32, 32], // size of the icon
 });
 
 // Icones pour le ski
 icones_selec['ski'] = L.icon({
   iconUrl: 'icones/ski_select.png',
   iconSize: [71, 81],
-  iconAnchor:   [34, 70],
-  popupAnchor:  [0, -80]
+  iconAnchor: [34, 70],
+  popupAnchor: [0, -80]
 
 });
 
 // Ajouter les markers
-for (var k in points.features){
+for (var k in points.features) {
   var poi = points.features[k];
 
   var type_poi = poi.properties.Type;
   var icone_marqueur = icones[type_poi];
 
   var marqueur = L.marker(
-    [poi.geometry.coordinates[1], poi.geometry.coordinates[0]],
-    {icon: icone_marqueur,
-    zIndexOffset: type_poi == 'terminus' ? 100 : 0,
+    [poi.geometry.coordinates[1], poi.geometry.coordinates[0]], {
+      icon: icone_marqueur,
+      zIndexOffset: type_poi == 'terminus' ? 100 : 0,
     }
   ).addTo(mymap);
 
   poi.marqueur = marqueur;
   marqueur.poi = poi;
 
-    //Informations sur le marqueur
-  marqueur.on('click', function(e){
-      var poi = e.target.poi;
-      var html = '<h3>'+ poi.properties.nom + '</h3>'
-      + '<p>' + poi.properties.divers + '</p>'
-      let sct = '.trains';
+  //Informations sur le marqueur
+  marqueur.on('click', function(e) {
+        var poi = e.target.poi;
+        var html = '<h3>' + poi.properties.nom + '</h3>' +
+          '<p>' + poi.properties.divers + '</p>'
+        let sct = '.trains';
 
-    // informations si c'est une station de ski
-    if(poi.properties.Type == 'ski') {sct = '.ski';
-
-      html += ' <p>' + poi.properties.arretpp + '</p>'
-      + '<p>'+ poi.properties.journee + '</p>'
-      + '<a href=' + poi.properties.web + '> Site Web </a>'
-      + '<div class="container">'
-        + '<img src=' + poi.properties.img + ' alt="ski", class = "image" >'
-        + '<div class="overlay">'
-          + ' <div class="text">'+ poi.properties.imgnom + '</div>'
-          + ' <div class="lien"> <em>'+ poi.properties.source + '<a href=' + poi.properties.lien + '>Lien </a> </em></div>'
-      +   '</div>'
-      +  '</div>';
-    };
-
+        // informations si c'est une station de ski
+        if (poi.properties.Type == 'ski') {
+          sct = '.ski';
+          html += ' <p>' + poi.properties.arretpp + '</p>' +
+            '<p>' + poi.properties.journee + '</p>' +
+            '<a href=' + poi.properties.web + '> Site Web </a>' +
+            '<div class="container">' +
+            '<img src=' + poi.properties.img + ' alt="ski", class = "image" >' +
+            '<div class="overlay">' +
+            ' <div class="text">' + poi.properties.imgnom + '</div>' +
+            ' <div class="lien"> <em>' + poi.properties.source + '<a href=' + poi.properties.lien + '>Lien </a> </em></div>' +
+            '</div>' +
+            '</div>';
+        };
     // afficher l'icone de la ligne selon la ligne
-    if (poi.properties.ligne == 'Ligne : Aigle - Champéry') {sct = '.trains';
+    if (poi.properties.ligne == 'Ligne : Aigle - Champéry') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="l126"></p>'
     }
-    if (poi.properties.ligne == 'Ligne: Aigle - Leysin') {sct = '.trains';
+    if (poi.properties.ligne == 'Ligne: Aigle - Leysin') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="l125"></p>'
     }
-    if (poi.properties.ligne == 'Ligne: Aigle - Le Sépey - Les Diablerets') {sct = '.trains';
+    if (poi.properties.ligne == 'Ligne: Aigle - Le Sépey - Les Diablerets') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="l124"></p>'
     }
-    if (poi.properties.ligne == 'Ligne: Bex - Gryon - Villars-sur-Ollon') {sct = '.trains';
+    if (poi.properties.ligne == 'Ligne: Bex - Gryon - Villars-sur-Ollon') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="l127"></p>'
     }
-    if (poi.properties.ligne == 'Ligne: Villars-sur-Ollon - Col-de-Bretaye') {sct = '.trains';
+    if (poi.properties.ligne == 'Ligne: Villars-sur-Ollon - Col-de-Bretaye') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="l128"></p>'
     }
 
     // afficher les îcones pour les arrêts qui sont sur plusieurs lignes
-    if (poi.properties.id == 'aigle') {sct = '.trains';
+    if (poi.properties.id == 'aigle') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="laigle"></p>'
     }
-    if (poi.properties.id == 'villarssurollon') {sct = '.trains';
+    if (poi.properties.id == 'villarssurollon') {
+      sct = '.trains';
       html += '<p>' + poi.properties.ligne + '<img src=' + poi.properties.limg + ' alt="ligne", class="lvillars"></p>'
     }
 
 
-      document.querySelector(sct).innerHTML = html;
+    document.querySelector(sct).innerHTML = html;
 
     //remettre les icones de base
-    for (var k in points.features){
-        var p = points.features[k];
+    for (var k in points.features) {
+      var p = points.features[k];
 
       var type_p = p.properties.Type;
       var icone_p = icones[type_p];
@@ -179,7 +188,9 @@ for (var k in points.features){
 
   // Attacher l'étiquette au marqueur
   marqueur.bindTooltip(poi.properties.nom, {
-    offset: [7, 0], direction: 'right', className: 'etiquette'
+    offset: [7, 0],
+    direction: 'right',
+    className: 'etiquette'
   }).addTo(mymap);
 
 };
